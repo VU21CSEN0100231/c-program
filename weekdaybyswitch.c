a724ddc5c4d6ac0f4cadbdfa257005fd49a397d3
VU21CSEN0100231 <94902521+VU21CSEN0100231@users.noreply.github.com>
2021-12-13 10:40:14 +0530

    Add files via upload

---
+++ weekdaybyswitch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int w;
+	scanf("%d",&w);
+	switch(w)
+	{
+		case 1:printf("monday");break;
+		case 2:printf("tuesday");break;
+		case 3:printf("wednesday");break;
+		case 4:printf("thursday");break;
+		case 5:printf("friday");break;
+		case 6:printf("saturday");break;
+		case 7:printf("sunday");break;
+		default:printf("not a week day");
+	}
+}
