82cefe831a483a86c347b5a486aaa74dac47ae7b
VU21CSEN0100231 <94902521+VU21CSEN0100231@users.noreply.github.com>
2021-12-20 10:43:21 +0530

    Add files via upload

---
+++ naturalnumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int i,n,m,sum;
+	scanf("%d",&n);
+	sum=0;
+	i=1;
+	while(i<=n);
+	{
+	sum=sum+i;
+	i=i+1;
+	}
+	printf("%d",i);
+}
---
+++ naturalnumbers1t10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	int i=1;
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	
+}
