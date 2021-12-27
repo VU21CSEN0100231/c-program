bc757b7e0bd6bdacae3a25e77b017a38bc5c845a
VU21CSEN0100231 <94902521+VU21CSEN0100231@users.noreply.github.com>
2021-12-27 10:04:21 +0530

    Add files via upload

---
+++ prime no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int r=0,c=0,n;
+	int d=2;
+	scanf("%d",&n);
+	while(d<=(n/2))
+	{
+		r=n%d;
+		if(r==0)
+		c++;
+		d++;
+	}
+	if(c==0)
+	printf("it is a prime number");
+	else
+	printf("it is not a prime number");
+}
